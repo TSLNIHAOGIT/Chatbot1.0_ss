49adce78bb981e58e6bb591351ed23ccdcba470a
weiluo <luoweiforever@gmail.com>
2018-06-19 01:44:42 -0400

    add run per browser

--- API/v1/frontend/app/run.py
+++ API/v1/frontend/app/run.py
@@ -19,16 +19,17 @@ import numpy as np
 import time
 import re
 import requests
+import uuid
 
 ##################################
 url = 'http://localhost:8889/chatbotv1'
 
 
-def get_data(sentence):
-    
+def get_data(sentence, uid):
+    sessionId = cache.uid_dict[uid]
     response = requests.get(url, timeout=10, params={'data':sentence, 
                                                      'action':'chat', 
-                                                     'sessionId':cache.sessionId})
+                                                     'sessionId':sessionId})
     
     if response.status_code == 200:
         req = response.json()
@@ -50,16 +51,37 @@ def index0():
                            # "index0.html",
                            "index.html",
                            )
+
+@app.route("/createCookie", methods = ['POST'])
+def createCookie():
+    if request.method == 'POST':
+        
+        cookieid = request.form.get('cookie');
+        if cookieid:
+            print('already exists')
+            print(cookieid)
+            return('')
+        else:
+            print('new user')
+            uid = str(uuid.uuid1())
+            print('create cookie {}'.format(uid))
+            return(str(uuid.uuid1()))
+
+
+
 @app.route("/predict", methods= ["POST"])
 def background_process():
     if request.method == 'POST':
         try:
             query = request.form.get('query')#前端查询的内容
+            uid = request.form.get('uid')
+            print(uid)
             if query:
 
+                    print('query',query)
                     # time.sleep(5)
-                    result = get_data(query)
-        
+                    result = get_data(query, uid)
+                    print('result',result)
                     mongo_db.save_query(query, str(result))
                     print('saving to mongo db successfully!')
                     return str(result)
@@ -76,14 +98,14 @@ def background_process():
             if 'duplicate' in str(e):
                 e_str = e.details['errmsg']
                 dup_id=re.search('\{ : "(.*)" \}',e_str).group(1)
-                print('query is the same, save use the same _id_')
+                print('query duplicated! Use same id to save')
                 mongo_db.update_dup_query( dup_id, str(result))
                 return str(result)
 
             else:
                 print(e)
-                print('babbabababaabba')
                 return str('MM出故障啦')
+
 
     else:
         return 'ok'
@@ -91,25 +113,50 @@ def background_process():
     
 @app.route("/newConversation", methods= ["POST"])
 def new_conversation():
-    cache.new_conversation()
+    
     print('new conversation called!!!!!!!!!!!!!!!!!!!!!!!')
+    if request.method == 'POST':
+        cookie = request.form.get('uid');
+        if cookie:
+            print(cookie)
+            cache.new_conversation(cookie)
+            return cookie
+        else:
+            print('cookie lost')
+            return('error')
 
 class Cache:
     def __init__(self):
-        self.new_conversation()
+#         self.new_conversation()
+        self.uid_dict = {}
             
     def new_conversation(self):
         req = requests.get(url, timeout=10, params={'action':'create'})
         if req.status_code == 200:
             msg = req.json()
             if msg['status'] == 'successful':
+                
                 self.sessionId = msg['message']['sessionId']
                 print(self.sessionId)
             else:
-         
+                print(req.content)
                 raise ValueError('cannot get new sessionId, cannot start chat')
         else:
-            print('request status code error: {}'.format(req.status_code))
+            print(req.content)
+            
+    def new_conversation(self, uid):
+        req = requests.get(url, timeout=10, params={'action':'create'})
+        if req.status_code == 200:
+            msg = req.json()
+            if msg['status'] == 'successful':
+                sessionId = msg['message']['sessionId']
+                self.uid_dict[uid] = sessionId
+                print(self.uid_dict[uid])
+            else:
+                print(req.content)
+                raise ValueError('cannot get new sessionId, cannot start chat')
+        else:
+            print(req.content)
         
 
 
