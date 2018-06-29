c4f72e900090b0abc2a51853185ccc7dff92e3b5
weiluo <luoweiforever@gmail.com>
2018-06-29 11:19:57 -0400

    change csv

---
+++ MLModel/code/TreeModelV2/test.py
@@ -0,0 +1,19 @@
+class test:
+    """
+    测试
+    """
+    def __init__(self):
+        """
+        测试1
+        """
+        pass
+        
+    def printer(self):
+        """
+        测试打印
+        """
+        print('inside printer')
+        
+if __name__ == "__main__":
+    c = test()
+    c.printer()
